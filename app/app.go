f3828247f31217ea8e4512a75877a242ec5b4753
George Rogers <george.f.rogers@gmail.com>
2017-02-21 10:57:25 -0600

    update

--- app/app.go
+++ app/app.go
@@ -20,7 +20,7 @@ var baseTpl = template.Must(template.ParseFiles("tpl/base.tpl"))
 var indexTpl = template.Must(template.Must(baseTpl.Clone()).ParseFiles("tpl/index.tpl"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	indexTpl.Execute(w, articles)
+	indexTpl.Execute(w, models.ArticleMap)
 }
 
 var articleTpl = template.Must(template.Must(baseTpl.Clone()).ParseFiles("tpl/article.tpl"))
